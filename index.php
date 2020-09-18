e7fe386230b55c399474d99ab3dcef1ff549e827
wilker22 <wilker.silva@codevasf.gov.br>
2020-09-18 17:39:05 -0300

    Tpladmin e AdminLte integrado

--- index.php
+++ index.php
@@ -4,15 +4,24 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
-
+use Hcode\PageAdmin;
 
 $app = new \Slim\Slim();
 
 $app->config('debug', true);
 
+//route site index
 $app->get('/', function() {
     
 	$page = new Page();
+	$page->setTpl("index");
+
+});
+
+//route admin
+$app->get('/admin', function() {
+    
+	$page = new PageAdmin();
 	$page->setTpl("index");
 
 });
